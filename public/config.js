1333c799d458cd2bc027158d9848d985113181eb
KiranBalasoPatil3052006 <kiranbalasopatil33@gmail.com>
2025-09-07 22:32:52 +0530

    Update config.js with backend URL

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@ const environment = 'development'; // 'development' or 'production'
 
 const URLS = {
   development: "http://localhost:5000",
-  production: "https://smartcart-2lvm.onrender.com"
+  production: "https://smartz.onrender.com"
 };
 
 const BASE_URL = URLS[environment];
